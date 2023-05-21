7739d29a1dfb484a21269cbaa2728bf79b8825dd
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-21 16:56:06 -0300

    section 7

--- Hashes.md
+++ Hashes.md
@@ -9,3 +9,14 @@ HGETALL hash
 DEL hash
 
 HDEL hash key
+
+## Reasons to Store as Hash
+- The record has many attributes
+- A collection of these records have to be sorted many different ways
+- Often need to access a single record at a time
+
+## Don't Use Hashes When
+- The record is only for counting or enforcing uniqueness
+- Record stores only one or two attributes
+- Used only for creating relations between different records
+- The record is only used for time series data
--- rbay/src/services/queries/items/items.ts
+++ rbay/src/services/queries/items/items.ts
@@ -1,7 +1,26 @@
+import { itemsKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
+import { genId } from '$services/utils';
+import { deserialize } from './deserialize';
+import { serialize } from './serialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id));
+
+	if (Object.keys(item).length < 1) return null;
+
+	return deserialize(id, item);
+};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const id = genId();
+
+	const serialized = serialize(attrs);
+
+	await client.hSet(itemsKey(id), serialized);
+
+	return id;
+};
