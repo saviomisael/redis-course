4421d779be97feb658e968ef990f8759d1e2d06a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-21 14:32:58 -0300

    section 6

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -1,5 +1,18 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+  await client.hSet('car', {
+    color: 'red',
+    year: 1950
+  })
+
+  const car = await client.hGetAll('car')
+
+  const car513 = await client.hGetAll('car#513') // returns {}
+
+  const company513 = await client.get('company#513') // returns null
+
+  console.log(car, car513, company513, Object.keys(car513).length > 0)
+};
 run();
